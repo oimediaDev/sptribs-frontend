42946ab4816ba196e31251dd722acbeb8931c87f
muradalamv1 <murad.alam@version1.com>
2023-05-02 10:48:37 +0100

    Url added for every page

--- src/main/steps/common/common.content.ts
+++ src/main/steps/common/common.content.ts
@@ -10,7 +10,7 @@ const en = {
   applyForAdoption: 'Apply for a Service',
   applyForDissolution: 'Apply for a Service',
   feedback:
-    "<b> Help us improve this service </b><br>This is a new service. Help us for others to improve it by <a  class='govuk-link' href='https://www.smartsurvey.co.uk/s/Specials_Feedback/?pageurl=currentUrl'target=_blank>giving your feedback</a>",
+    "<b> Help us improve this service</b><br>This is a new service. Help us for others to improve it by <a  class='govuk-link' href='https://www.smartsurvey.co.uk/s/Specials_Feedback/?pageurl=currentUrl'target=_blank>giving your feedback</a>",
   languageToggle: '<a href="?lng=cy" class="govuk-link language">Cymraeg</a>',
   govUk: 'GOV.UK',
   back: 'Back',
